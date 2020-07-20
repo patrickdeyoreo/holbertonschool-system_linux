8abbb963a036259ce2a5d144e37c7ac9c7e531ff
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-07-20 09:56:09 -0700

    Changed entry base

--- 0x00-ls/entry.h
+++ 0x00-ls/entry.h
@@ -8,42 +8,44 @@
 
 #include "queue.h"
 
-/**
- * union entry_s - an entry describing either a file or a directory
- * @common: information common to all files
- * @subtree: common file information followed by directory-specific information
- */
-typedef union entry_s
-{
-	struct file_s *common;
-	struct dir_s *subtree;
-} entry_t;
+#define NAME_SIZE 256
 
 /**
  * struct file_s - an entry describing a file
- * @entry: the directory entry as returned by readdir
- * @stat: status of the file as returned by lstat
+ * @name: name of the file
+ * @info: file information
  */
 typedef struct file_s
 {
-	struct dirent *entry;
-	struct stat *stat;
+	char name[NAME_SIZE];
+	struct stat info;
 } file_t;
 
 
 /**
  * struct dir_s - an entry describing a directory
- * @entry: the directory entry as returned by readdir
- * @stat: status of the file as returned by lstat
- * @contents: contents of the directory
- * @subdirs: subdirectories queued for processing
+ * @name: name of the directory
+ * @info: directory information
+ * @entries: directory entries
+ * @subdirs: subdirectory list
  */
 typedef struct dir_s
 {
-	struct dirent *entry;
-	struct stat *stat;
-	entry_t **contents;
+	char name[NAME_SIZE];
+	struct stat info;
+	union entry_s **entries;
 	queue_node_t *subdirs;
 } dir_t;
+
+/**
+ * union entry_s - an entry describing a directory entry
+ * @file: common file information
+ * @dir: common file information with additional directory-specific information
+ */
+typedef union entry_s
+{
+	file_t file;
+	dir_t dir;
+} entry_t;
 
 #endif /* _ENTRY_H_ */
