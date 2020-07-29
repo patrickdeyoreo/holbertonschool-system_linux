6236fee06c612c8b98248b331a619374705617b0
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-07-29 13:23:21 -0700

    Added completed task 0

--- 0x01-getline/laps.c
+++ 0x01-getline/laps.c
@@ -1,5 +1,79 @@
-#include <string.h>
 #include "laps.h"
+
+/**
+ * _strlen - measure the length of a string
+ * @s: the string to measure
+ * Return: the length of the string
+ */
+static size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	if (s)
+	{
+		while (*s++)
+			len += 1;
+	}
+	return (len);
+}
+
+/**
+ * printint - write an integer to stdout
+ * @n: the integer
+ */
+static void printint(int n)
+{
+	char chr = '0' + (n < 0 ? -1 : 1) * (n % 10);
+
+	if (n < 0)
+		write(STDOUT_FILENO, "-", 1);
+	if (n / 10)
+		printint((n < 0 ? -1 : 1) * (n / 10));
+
+	write(STDOUT_FILENO, &chr, 1);
+}
+
+/**
+ * print_race_state - print the number of laps each car has completed
+ * @head: a pointer to the head of a list of cars
+ */
+static void print_race_state(const s_hash_node_t *head)
+{
+	if (head)
+	{
+		PUTS("Race state\n");
+		do {
+			PUTS("Car ");
+			printint(head->id);
+			PUTS(" [");
+			printint(head->value);
+			PUTS(" laps]\n");
+		} while ((head = head->s_next));
+	}
+}
+
+/**
+ * free_race_state - free memory allocated for the race state
+ * @state: a pointer to the race state
+ */
+static void free_race_state(s_hash_table_t *state)
+{
+	s_hash_node_t *tmp = NULL;
+	size_t index = 0;
+
+	if (state)
+	{
+		while ((tmp = state->sorted))
+		{
+			state->sorted = tmp->s_next;
+			free(tmp);
+		}
+		while (index < TABLE_SIZE)
+		{
+			state->table[index++] = NULL;
+		}
+	}
+}
 
 /**
  * race_state - track the number of laps made by several cars in a race
@@ -15,17 +89,46 @@
  * sorted by id, where X is the id and Y is the number of laps completed. If
  * this function is called with size = 0, it will free all allocated memory.
  */
-void race_state(int *id __attribute__((unused)), size_t size)
+void race_state(int *id, size_t size)
 {
 	static s_hash_table_t state = {0};
-	s_hash_node_t *tmp = NULL;
+	s_hash_node_t **ref = NULL, *tmp = NULL;
+	int *stop = NULL;
 
-	if (size == 0)
+	if (size)
 	{
-		while ((tmp = state.sorted))
+		for (stop = id + size; id < stop; id += 1)
 		{
-			state.sorted = tmp->next;
-			free(tmp);
+			ref = &state.table[HASH(*id)];
+			while (*ref && (*ref)->id != *id)
+				ref = &(*ref)->next;
+			if (*ref)
+			{
+				(*ref)->value += 1;
+			}
+			else
+			{
+				PUTS("Car ");
+				printint(*id);
+				PUTS(" joined the race\n");
+				tmp = *ref = malloc(sizeof(*tmp));
+				if (tmp)
+				{
+					tmp->next = NULL;
+					tmp->id = *id;
+					tmp->value = 0;
+				}
+				ref = &state.sorted;
+				while (*ref && (*ref)->id < tmp->id)
+					ref = &(*ref)->s_next;
+				tmp->s_next = *ref;
+				*ref = tmp;
+			}
 		}
+		print_race_state(state.sorted);
+	}
+	else
+	{
+		free_race_state(&state);
 	}
 }
--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -3,36 +3,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define HASH_TABLE_SIZE 127
+#define TABLE_SIZE 127
 
-#define HASH(x) ((x) % HASH_TABLE_SIZE)
+#define HASH(x) ((x) % TABLE_SIZE)
+
+#define MESSAGE_ADDED_ID(x) ("Car " (x) " joined the race")
+#define MESSAGE_ID_LAPS(x, y) ("Car " (x) " [" (y) " laps]")
+
+#define PUTS(s) write(STDOUT_FILENO, (s), _strlen(s))
 
 /**
- * struct s_hash_node_s - a singly-linked and doubly-linked list node
+ * struct s_hash_node_s - a list node for two separate singly-linked lists
  * @next: the address of the next node in a singly-linked hash table chain
  * @s_next: the address of the next node in a sorted doubly-linked list
- * @s_prev: the address of the prev node in a sorted doubly-linked list
- * @id: the id (key) of the data
- * @value: the value of the data
+ * @id: the id (key) of the element
+ * @value: the value of the element
  */
 typedef struct s_hash_node_s
 {
 	struct s_hash_node_s *next;
 	struct s_hash_node_s *s_next;
-	struct s_hash_node_s *s_prev;
 	int id;
 	int value;
 } s_hash_node_t;
 
 /**
- * struct s_hash_table_s - a hash table and a sorted doubly-linked list
- * @table: an array of table elements chained for collision resolution
- * @sorted: a pointer to the first item in a sorted doubly-linked list
+ * struct s_hash_table_s - a hash table and a sorted singly-linked list
+ * @table: an array of tabledoubly elements chained for collision resolution
+ * @sorted: a pointer to the first item in a sorted singly-linked list
  */
 typedef struct s_hash_table_s
 {
-	struct s_hash_node_s *table[HASH_TABLE_SIZE];
+	struct s_hash_node_s *table[TABLE_SIZE];
 	struct s_hash_node_s *sorted;
 } s_hash_table_t;
 
