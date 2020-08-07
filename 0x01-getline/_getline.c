c86049d512f5a6bcb5b339cf00243bff5295e73b
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-08-06 23:50:44 -0700

    explicit void * cast

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -87,7 +87,7 @@ char *_getline(const int fd)
 		{
 			eol = _strnchr(buf.next, '\n', buf.remaining);
 			new = eol == -1 ? buf.remaining : eol;
-			aux = line ? _realloc(line, size, size + new) : malloc(new + 1);
+			aux = line ? _realloc((void *) line, size, size + new) : malloc(new + 1);
 			if (aux == NULL)
 			{
 				free(line);
