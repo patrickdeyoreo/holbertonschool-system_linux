3703ca9411aad2c31078cbd2048e5463102fa137
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-08-27 02:02:30 -0700

    Fixed stdout/stderr IO issues in read_write_heap

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -3,6 +3,7 @@
 
 import argparse
 import collections
+import contextlib
 import functools
 import itertools
 import operator
@@ -13,18 +14,30 @@ import sys
 class MapEntry:
     """A process memory-map entry."""
     # pylint: disable=too-few-public-methods
+
     pmap = {'r': 4, 'w': 2, 'x': 1, 'p': 0, 's': 0, '-': 0}
     Dev = collections.namedtuple('dev', ('major', 'minor'))
 
-    def __init__(self, address, perms, offset, dev, inode, pathname):
+    def __init__(self, address, perms, offset, dev, inode, pathname=None):
         """Initialize a memory-map entry."""
         # pylint: disable=too-many-arguments
         self.address = tuple(int(x, 0x10) for x in address.split('-', 1))
         self.perms = functools.reduce(operator.or_, map(self.pmap.get, perms))
         self.offset = int(offset, 0x10)
-        self.dev = self.Dev(dev.split(':', 1))
+        self.dev = self.Dev(*dev.split(':', 1))
         self.inode = int(inode) or None
         self.pathname = pathname
+
+
+@contextlib.contextmanager
+def stdout_as_stderr():
+    """Temporarily redirect stderr to stdout."""
+    stderr = sys.stderr
+    sys.stderr = sys.stdout
+    try:
+        yield
+    finally:
+        sys.stderr = stderr
 
 
 def parse_args():
@@ -40,7 +53,7 @@ def read_maps(**kwgs):
     """Get the memory-map of a process."""
     split = operator.methodcaller('split')
     with open('/proc/{pid}/maps'.format(**kwgs), 'r') as istream:
-        return itertools.starmap(MapEntry, map(split, istream))
+        return itertools.starmap(MapEntry, map(split, istream.readlines()))
 
 
 def read_heap(entry, **kwgs):
@@ -64,19 +77,22 @@ def write_heap(entry, offset, **kwgs):
 
 def main():
     """Hack the virtual memory."""
-    with sys.stdout as sys.stderr:
-        kwgs = vars(parse_args())
-    maps = {entry.pathname: entry for entry in read_maps(**kwgs)}
-    heap = read_heap(maps['[heap]'], **kwgs)
-    write_heap(maps['[heap]'], search_heap(heap, **kwgs), **kwgs)
+    try:
+        with stdout_as_stderr():
+            kwgs = vars(parse_args())
+    except SystemExit:
+        return 1
+    try:
+        maps = {entry.pathname: entry for entry in read_maps(**kwgs)}
+        heap = read_heap(maps['[heap]'], **kwgs)
+        offset = search_heap(heap, **kwgs)
+        write_heap(maps['[heap]'], offset, **kwgs)
+    except (IOError, KeyError, ValueError) as exc:
+        print(exc, file=sys.stderr)
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
-    # pylint: disable=broad-except
-    try:
-        sys.exit(main())
-    except SystemExit:
-        sys.exit(1)
-    except Exception:
-        print(sys.exc_info())
-        sys.exit(1)
+    sys.exit(main())
