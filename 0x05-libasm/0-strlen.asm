f24c2e7210eb058c06bb12b09c6177f5d0c9078c
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2020-09-22 18:28:48 -0700

    Removed extra register management

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -11,20 +11,17 @@ BITS 64
 	; Return: the length of the string pointed to by str
 
 asm_strlen:
-			; the following two lines are equivalent to ENTER
-	push rbp	; save frame pointer
-	mov rbp, rsp	; create new frame
 
-	push rsi	; save @rsi
-	push rdx	; save @rdx
+	push rbp		; save frame pointer
+	mov rbp, rsp		; create new frame
 
-	mov rsi, 0h	; copy 0 into @rsi
+	mov rsi, 0h		; copy 0 into @rsi
 
 asm_strlen_loop:
 
-	mov dl,	[rdi + rsi]	; copy the byte at @rdi + @rsi into @dl
+	mov dl, [rdi + rsi]	; copy the byte at @rdi + @rsi into @dl
 
-	cmp dl,	0h		; compare @dl with 0
+	cmp dl, 0h		; compare @dl with 0
 	je asm_strlen_end	; jump to "end" if @dl is equal to 0
 
 	inc rsi			; increment offset
@@ -32,15 +29,11 @@ asm_strlen_loop:
 
 asm_strlen_end:
 
-	mov rax, rsi	; return the value stored in @rsi
+	mov rax, rsi		; return the value stored in @rsi
 
-	pop rdx		; restore @rdx
-	pop rsi		; restore @rsi
+	mov rsp, rbp		; restore stack pointer
+	pop rbp			; restore frame pointer
 
-			; the following two lines are equivalent to LEAVE
-	mov rsp, rbp	; restore stack pointer
-	pop rbp		; restore frame pointer
-
-	ret		; pop @rip
+	ret			; pop @rip
 
 ; vi: set noet ft=nasm sts=0 sw=8 ts=8 :
