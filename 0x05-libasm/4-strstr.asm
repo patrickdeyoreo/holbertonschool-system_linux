dd4de650a64f2c716008a8d8e4d325c22a1ad9bb
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-09-23 23:13:01 -0700

    Successfully debugged asm_strstr

--- 0x05-libasm/4-strstr.asm
+++ 0x05-libasm/4-strstr.asm
@@ -12,67 +12,68 @@ BITS 64
 	; Return: pointer to the beginning of the located substring
 
 asm_strstr:
-					; the following two lines are equivalent to ENTER
+
 	push rbp			; save frame pointer
 	mov rbp, rsp			; create new frame
 
-	push rdx			; save @rdx on the stack
+	xor rax, rax			; zero-initialize @rax
+
+	sub rsp, 16			; increase the size of the stack by 16 bytes
+
+asm_strstr_loop:
+
+	mov dl, [rdi]			; copy the byte at @rdi into @dl
+
+	cmp dl, [rsi]			; compare the byte at @rsi with @dl
+	je asm_strstr_match		; jump to "match" if @dl is equal to the byte at @rsi
+
+	cmp dl, 0			; compare 0 with @dl
+	je asm_strstr_end		; jump to "end" if @dl is equal to 0
+
+asm_strstr_continue:
+
+	inc rdi				; increment @rdi
+
+	jmp asm_strstr_loop		; jump to the beginning of the loop
 
 asm_strstr_match:
 
-	push rdi			; save @rdi on the stack
-	push rsi			; save @rsi on the stack
+	mov [rsp - 8], rdi		; save @haystack on the stack
+	mov [rsp], rsi			; save @needle on the stack
 
 asm_strstr_match_loop:
 
-	mov dl, byte [rsi]		; copy the byte at @rsi into @dl
+	mov dl, [rsi]			; copy the byte at @rsi into @dl
 
-	cmp dl, byte 0h			; compare 0 with @dl
-	je asm_strstr_found		; jump to "found" if @dl is equal to 0
+	cmp dl, 0			; compare 0 with @dl
+	je asm_strstr_match_found	; jump to "match_found" if @dl is equal to 0
 
-	cmp dl, byte [rdi]		; compare the byte at @rdi with @dl
-	jne asm_strstr_continue		; jump to "continue" if @dl is not equal to the byte at @rdi
+	cmp dl, [rdi]			; compare the byte at @rdi with @dl
+	jne asm_strstr_match_break	; jump to "match_break" if @dl is not equal to the byte at @rdi
 
 	inc rdi				; increment @rdi
 	inc rsi				; increment @rsi
 
 	jmp asm_strstr_match_loop	; jump to "match_loop" if the byte at @rdi is equal to @dl
 
-asm_strstr_found:
+asm_strstr_match_found:
 
-	pop rsi				; restore @rsi from the stack
-	pop rax				; restore @rdi from the stack
+	mov rax, [rsp - 8]		; return @rdi from the stack
 
 	jmp asm_strstr_end		; jump to "end"
 
-asm_strstr_continue:
+asm_strstr_match_break:
 
-	pop rsi				; restore @rsi from the stack
-	pop rdi				; restore @rdi from the stack
+	mov rdi, [rsp - 8]		; restore @rdi from the stack
+	mov rsi, [rsp]			; restore @rsi from the stack
 
-asm_strstr_loop:
-
-	mov dl, byte [rdi]		; copy the byte at @rdi into @dl
-
-	cmp dl, byte 0h			; compare 0 with @dl
-	je asm_strstr_none		; jump to "end" if @dl is equal to 0
-
-	cmp dl, byte [rsi]		; compare the byte at @rsi with @dl
-	je asm_strstr_match		; jump to "match" if @dl is equal to the byte at @rsi
-
-	inc rdi				; increment @rdi
-
-	jmp asm_strstr_loop		; jump to the beginning of the loop
-
-asm_strstr_none:
-
-	mov rax, 0h			; copy 0 into @rdi
+	cmp byte [rdi], 0		; compare 0 with the byte at @rdi
+	jne asm_strstr_continue		; jump to "continue"
 
 asm_strstr_end:
 
-	pop rdx				; restore @rdx from the stack
+	add rsp, 16			; decrease the size of the stack by 16 bytes
 
-					; the following two lines are equivalent to LEAVE
 	mov rsp, rbp			; restore stack pointer
 	pop rbp				; restore frame pointer
 
