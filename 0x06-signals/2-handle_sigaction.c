3ad33927e692e2e524e383e577fec92c797d978a
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-09-27 21:31:50 -0700

    Debugging task 2

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -19,9 +19,7 @@ int handle_sigaction(void)
 	sigaction_t sigint_action;
 
 	sigint_action.sa_handler = sigint_handler;
-	sigint_action.sa_sigaction = NULL;
 	sigint_action.sa_flags = 0;
-	sigint_action.sa_restorer = NULL;
 	sigemptyset(&sigint_action.sa_mask);
 
 	return (sigaction(SIGINT, &sigint_action, NULL) < 0 ? -1 : 0);
