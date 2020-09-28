e4fa366b5cd36ac5132b33b6a1c4d134e5cb305c
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-09-27 21:23:54 -0700

    Adding task 2

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -1,11 +1,26 @@
 #include "signals.h"
+/**
+ * sigint_handler - print a message upon receipt of SIGINT
+ * @signum: signal number
+ */
+static void sigint_handler(int signum)
+{
+	printf("Gotcha! [%d]\n", signum);
+}
+
 
 /**
- * handle_sigaction -
+ * handle_sigaction - set a handler for SIGINT
  *
- * Return: 
+ * Return: -1 on error, 0 otherwise
  */
 int handle_sigaction(void)
 {
+	sigaction_t sigint_action = {0};
 
+	sigint_action.sa_handler = sigint_handler;
+
+	sigemptyset(&sigint_action.sa_mask);
+
+	return (sigaction(SIGINT, &sigint_action, NULL) < 0 ? -1 : 0);
 }
