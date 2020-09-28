60de91c339f07f9fabfef998d79742642ce5db97
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-09-27 21:29:43 -0700

    Task 2 compiles

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -16,10 +16,12 @@ static void sigint_handler(int signum)
  */
 int handle_sigaction(void)
 {
-	sigaction_t sigint_action = {0};
+	sigaction_t sigint_action;
 
 	sigint_action.sa_handler = sigint_handler;
-
+	sigint_action.sa_sigaction = NULL;
+	sigint_action.sa_flags = 0;
+	sigint_action.sa_restorer = NULL;
 	sigemptyset(&sigint_action.sa_mask);
 
 	return (sigaction(SIGINT, &sigint_action, NULL) < 0 ? -1 : 0);
