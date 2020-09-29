cecd7ed30b45562cdfe9406bff5a96f70e2476e7
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-09-28 21:13:17 -0700

    Adding task 5

--- 0x06-signals/5-signal_describe.c
+++ 0x06-signals/5-signal_describe.c
@@ -1,11 +1,19 @@
 #include "signals.h"
 
 /**
- * pid_exist -
+ * main - print a description of a given signal
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: 
+ * Return: Upon failure, return EXIT_FAILURE. Otherwise, return EXIT_SUCCESS.
  */
-int pid_exist(pid_t pid)
+int main(int argc, char **argv)
 {
-
+	if (argc != 2)
+	{
+		printf("Usage: %s <signum>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	printf("%s\n", strsignal(strtol(argv[1], NULL, 10)));
+	return (EXIT_SUCCESS);
 }
