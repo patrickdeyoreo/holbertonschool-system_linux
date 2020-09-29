935312c52fa942e4e3997cc3801e1c0d924bce8c
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-09-28 21:01:09 -0700

    Adding task 10

--- 0x06-signals/10-pid_exist.c
+++ 0x06-signals/10-pid_exist.c
@@ -1,11 +1,12 @@
 #include "signals.h"
 
 /**
- * handle_pending -
+ * pid_exist - check if a process with the given PID exists
+ * @pid: pid of target process
  *
- * Return: 
+ * Return: 1 if the process with the given PID exists, 0 otherwise
  */
-int handle_pending(void (*handler)(int))
+int pid_exist(pid_t pid)
 {
-
+	return (kill(pid, 0) == 0);
 }
