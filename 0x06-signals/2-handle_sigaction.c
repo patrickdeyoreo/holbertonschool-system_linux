8e9622f935d6980655d28f94bf3f5189e23ef70e
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-10-01 09:31:32 -0700

    Using printf in signal handlers for simplicity

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -1,32 +1,12 @@
 #include "signals.h"
 
-#define putstr(s) fputs((s), stdout)
-
-/**
- * putint - print an integer on standard output
- * @n: the integer to print
- */
-static void putint(int n)
-{
-	int sign = n > -1 ? 1 : -1;
-	char chr = (n % 10) * sign + '0';
-
-	if (n < 0)
-		putchar('-');
-	if (n / 10)
-		putint((n / 10) * sign);
-	putchar(chr);
-}
-
 /**
  * sigint_handler - print a message upon receipt of SIGINT
  * @signum: signal number
  */
 static void sigint_handler(int signum)
 {
-	putstr("Gotcha! [");
-	putint(signum);
-	putstr("]\n");
+	printf("Gotcha! [%d]\n", signum);
 }
 
 /**
