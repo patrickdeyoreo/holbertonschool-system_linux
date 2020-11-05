84bd7d4329ec9f1cbce3c2eb55239ef4f03e4fc9
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-11-04 20:46:33 -0800

    Removed header file and fixed task 1

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -2,8 +2,6 @@
 
 #include <Python.h>
 
-#include "python.h"
-
 /**
  * print_python_list - print basic info about a Python list object
  *
@@ -11,20 +9,15 @@
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t i = 0;
-	Py_ssize_t n = 0;
+	Py_ssize_t i = 0, n = PyList_Size(p);
 	PyObject *o = NULL;
 
-	if (p)
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", n);
+	printf("[*] Allocated = %ld\n", ((PyListObject *) p)->allocated);
+	while (i < n)
 	{
-		n = PyList_Size(p);
-		printf("[*] Python list info\n");
-		printf("[*] Size of the Python List = %ld\n", n);
-		printf("[*] Allocated = %ld\n", Py_SIZE(p));
-		while (i < n)
-		{
-			o = PyList_GetItem(p, i);
-			printf("Element %ld: %s\n", i++, Py_TYPE(o)->tp_name);
-		}
+		o = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i++, Py_TYPE(o)->tp_name);
 	}
 }
--- 0x08_CPython/python.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PYTHON_H_
-#define _PYTHON_H_
-
-#include <Python.h>
-
-void print_python_list(PyObject *p);
-
-#endif /* _PYTHON_H_ */
