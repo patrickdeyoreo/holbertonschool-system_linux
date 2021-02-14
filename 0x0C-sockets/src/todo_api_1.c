59bee4b9046deb583ebd8a8e40bb60f47eaabcc7
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-02-13 23:42:53 -0800

    Refactored task 5 parsing

--- 0x0C-sockets/src/todo_api_1.c
+++ 0x0C-sockets/src/todo_api_1.c
@@ -24,10 +24,8 @@ void parse_request(char *request_buf)
 	const char *DELIMS = " \t\n\r";
 	char *request_save = NULL;
 	char *path_save = NULL;
-	char *params_save = NULL;
 	char *param_save = NULL;
 	char *path = NULL;
-	char *params = NULL;
 	char *param = NULL;
 	char *key = NULL;
 	char *value = NULL;
@@ -35,16 +33,15 @@ void parse_request(char *request_buf)
 	strtok_r(request_buf, DELIMS, &request_save);
 	path = strtok_r(NULL, DELIMS, &request_save);
 
-	printf("Path: %s\n", path);
 	strtok_r(path, "?", &path_save);
-	params = strtok_r(NULL, "", &path_save);
+	printf("Path: %s\n", path);
 
-	param = strtok_r(params, "&", &params_save);
+	param = strtok_r(NULL, "&", &path_save);
 	while (param)
 	{
 		key = strtok_r(param, "=", &param_save);
 		value = strtok_r(NULL, "", &param_save);
-		printf("Query: \"%s\" -> \"%s\"\n", key, value);
-		param = strtok_r(NULL, "&", &params_save);
+		printf("param: \"%s\" -> \"%s\"\n", key, value);
+		param = strtok_r(NULL, "&", &path_save);
 	}
 }
